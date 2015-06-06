30f10b0946ece191f6ceac4a353872572bfb50eb
tanigaki <inertial.mass01@gmail.com>
2015-06-06 11:19:53 +0900

    仕様2,3を実装

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define MAX_LEN 256 //1行あたり読み込める最大文字数
+
+void usage();
+
+int main(int argc, char *argv[])
 {
-	printf("オムライス\n");
+	FILE *fp;
+	char str[MAX_LEN];
+
+
+	if(argc < 2)
+	{
+		usage();
+		exit(1);
+	}
+
+
+	if( (fp = fopen(argv[1], "r")) == NULL)
+	{
+		printf("File Open Error!!\n");
+		exit(1);
+	}
+
+
+	while( fgets(str, MAX_LEN, fp) != NULL )
+	{
+		printf("%s\n", str);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+void usage()
+{
+	printf("usage: ./main filename\n");
 }
